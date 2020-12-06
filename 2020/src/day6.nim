f2d593837e76282a0b66b58f45478c0c934aad49
Daniel Flanagan <daniel@lyte.dev>
2020-12-05 23:20:53 -0600

    Clean up lines

--- 2020/src/day6.nim
+++ 2020/src/day6.nim
@@ -4,4 +4,6 @@ proc part1*(s: Stream): int =
   s.readAll().split("\n\n").mapIt(it.split("\n").foldl(a.union(b.toHashSet), initHashSet[char]()).len).foldl(a + b)
 
 proc part2*(s: Stream): int =
-  s.readAll().split("\n\n").mapIt(it.split("\n").filterIt(it != "").foldl(a.intersection(b.toHashSet), "abcdefghijklmnopqrstuvwxyz".toHashSet()).len).foldl(a + b)
+  s.readAll().split("\n\n").mapIt(
+    it.split("\n").filterIt(it != "").foldl(a.intersection(b.toHashSet), "abcdefghijklmnopqrstuvwxyz".toHashSet()).len
+  ).foldl(a + b)
