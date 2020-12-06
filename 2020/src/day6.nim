ecff477d6eee738404a999bdce3f162354e0cfcb
Daniel Flanagan <daniel@lyte.dev>
2020-12-05 23:20:16 -0600

    Part 2 for day 6 done as well

--- 2020/src/day6.nim
+++ 2020/src/day6.nim
@@ -1,8 +1,7 @@
 import streams, sets, strutils, sequtils
 
 proc part1*(s: Stream): int =
-  # s.readAll().split("\n\n").mapIt(it.split("\n").foldl(a.union(b.toHashSet), initHashSet)).len
   s.readAll().split("\n\n").mapIt(it.split("\n").foldl(a.union(b.toHashSet), initHashSet[char]()).len).foldl(a + b)
 
 proc part2*(s: Stream): int =
-  9
+  s.readAll().split("\n\n").mapIt(it.split("\n").filterIt(it != "").foldl(a.intersection(b.toHashSet), "abcdefghijklmnopqrstuvwxyz".toHashSet()).len).foldl(a + b)
