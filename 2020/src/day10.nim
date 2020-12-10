014890191ac7e879e7b1dc40da9b65e815224ddb
Daniel Flanagan <daniel@lyte.dev>
2020-12-10 01:08:23 -0600

    Day 10p1?

--- 2020/src/day10.nim
+++ 2020/src/day10.nim
@@ -1,7 +1,16 @@
-import streams
+import streams, input_helpers, algorithm, sequtils
 
 proc part1*(s: Stream): int =
-  0
+  var d1 = 0
+  var d3 = 1
+  var nums = @[0].concat(toSeq(s.asInts).sorted)
+  for i in 1..<nums.len:
+    case nums[i] - nums[i-1]:
+      of 1: inc d1
+      of 3: inc d3
+      else: discard
+  d1 * d3
+
 
 proc part2*(s: Stream): int =
   0
