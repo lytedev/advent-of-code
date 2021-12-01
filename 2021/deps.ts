5fe2f3343d656480a31d0cf2b6225a4746044e28
Daniel Flanagan <daniel@lyte.dev>
2021-12-01 10:49:24 -0600

    Update fs mod

--- 2021/deps.ts
+++ 2021/deps.ts
@@ -1,4 +1,4 @@
 export { readLines } from "https://deno.land/std@0.116.0/io/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.110.0/fs/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.116.0/fs/mod.ts";
 export * as path from "https://deno.land/std@0.116.0/path/mod.ts";
 export { readAll } from "https://deno.land/std@0.116.0/streams/conversion.ts";
