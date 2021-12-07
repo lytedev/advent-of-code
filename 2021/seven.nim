da61289cbb7c79e891f3b0200cf2342dadfc50ad
Daniel Flanagan <daniel@lyte.dev>
2021-12-07 12:01:34 -0600

    Ok i'm done DRYing

--- 2021/seven.nim
+++ 2021/seven.nim
@@ -1,17 +1,9 @@
 import ./common, std/[strutils, sequtils, strformat, sugar]
 
-proc crabFuel(c: seq[int]): int =
-  (0..c.foldl(max(a, b))).toSeq()
-    .reduce((r,t) => min(c.foldl(a + abs(b - t), 0), r), high(int))
+proc crabFuel(c: seq[int], f = (n: int) => n): int =
+  toSeq((1..c.foldl(max(a, b)))).reduce((r,t) => min(c.foldl(a + f(abs(b - t)), 0), r), high(int))
 
-proc triangleNumber(n: int): int = int((n * (n + 1)) / 2)
-
-proc crabMoreFuel(c: seq[int]): int =
-  (0..c.foldl(max(a, b))).toSeq()
-    .reduce((r,t) => min(c.foldl(a + abs(b - t).triangleNumber(), 0), r), high(int))
-
-doDay(7,
-  (day) => day.loadInputText().split(',').map(parseInt),
-  crabFuel,
-  crabMoreFuel,
+doDay(7, (n) => n.loadInputText().split(',').map(parseInt),
+  (n) => n.crabFuel(),
+  (n) => n.crabFuel((c: int) => (c * (c + 1)) div 2),
   @[16,1,2,0,4,2,7,1,2,14], 37, 168)
