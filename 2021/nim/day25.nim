fa083a7c0a047ec07c9c850c2edc73b6712654a1
Daniel Flanagan <daniel@lyte.dev>
2021-12-26 14:56:37 -0600

    Day 25 done

--- 2021/nim/day25.nim
+++ 2021/nim/day25.nim
@@ -5,16 +5,12 @@ proc p1(input: Lines): uint64 =
   var w = input[0].len
   var cukes = input
   while true:
-    echo result
     var nextCukes = cukes
-    echo nextCukes.join("\n")
-    echo ""
     let startHash = cukes.hash
     for y,line in cukes.pairs:
       for x,c in line.pairs:
         let tx = if x >= w - 1: 0 else: x + 1
         if c == '>' and cukes[y][tx] == '.':
-          echo &"{x}, {y} going right"
           nextCukes[y][x] = '.'
           nextCukes[y][tx] = '>'
     cukes = nextCukes
@@ -22,7 +18,6 @@ proc p1(input: Lines): uint64 =
       for x,c in line.pairs:
         let ty = if y >= h - 1: 0 else: y + 1
         if c == 'v' and cukes[ty][x] == '.':
-          echo &"{x}, {y} going down"
           nextCukes[y][x] = '.'
           nextCukes[ty][x] = 'v'
 
