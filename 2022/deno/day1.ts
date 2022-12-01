790f3ceaec3c626335e6067e8edfa4236d805429
Daniel Flanagan <daniel@lyte.dev>
2022-12-01 12:33:06 -0600

    Port common input fetcher helpers, cleanup, and add tests

--- 2022/deno/day1.ts
+++ 2022/deno/day1.ts
@@ -1,19 +1,47 @@
-const tDec = new TextDecoder();
-const input = tDec.decode(Deno.readFileSync("../inputs/day1.input"));
+import { inputLinesArray } from "./common.ts";
 
-console.log(
-  "final",
-  input.split("\n\n").map((p) => {
-    const ns = p.split("\n").map((s) => {
-      const n = parseInt(s, 10);
-      console.log("inner", s, n);
-      return n;
-    }).filter((n) => !isNaN(n));
-    if (ns.length < 1) return NaN;
-    const n = ns.reduce((prev, cur) => prev + cur, 0);
-    console.log("oter", p, n, ns);
-    return n;
-  }).filter((n) => !isNaN(n)).sort().reverse(),
-);
+function orderedCalories(lines: string[]) {
+  const calories: number[] = [0];
 
-console.log(71924 + 69893 + 68589);
+  lines.forEach((line) => {
+    if (line.trim() == "") {
+      calories.splice(0, 0, 0);
+    } else {
+      calories[0] += parseInt(line);
+    }
+  });
+  calories.sort((a, b) => b - a);
+  return calories;
+}
+
+const calories = orderedCalories(await inputLinesArray(1));
+console.log("Part 1:", calories[0]);
+console.log("Part 2:", calories.slice(0, 3).reduce((a, b) => a + b));
+
+const TEST_INPUT = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`.split("\n");
+
+Deno.test("orderedCalories", async (t) => {
+  const { assertEquals } = await import(
+    "https://deno.land/std@0.167.0/testing/asserts.ts"
+  );
+
+  const calories = orderedCalories(TEST_INPUT);
+  await t.step("part 1", () => assertEquals(calories[0], 24000));
+  await t.step(
+    "part 2",
+    () => assertEquals(calories.slice(0, 3).reduce((a, b) => a + b), 45000),
+  );
+});
